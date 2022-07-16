4597ddcdea98c6e37dfb6273fc93e4a1f55ee320
yamil <yamil@inbox.ru>
2022-07-16 18:36:28 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(
+                calc.newFormula()
+                        .addOperand(5)
+                        .addOperand(15)
+                        .calculate(Calculator.Operation.MULT)
+                        .result()
+        );
+        // Simple calculator
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
+
+    }
+}
